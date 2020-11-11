37f6f232c4b72d168615118e8db92e7f1282d42b
JayeshLohar <67799639+JayeshLohar@users.noreply.github.com>
2020-11-11 15:20:07 +0530

    Update project.py

--- PPL Mini Project 111910048/project.py
+++ PPL Mini Project 111910048/project.py
@@ -1,12 +1,15 @@
-import pyttsx3 # For speak() function
+import pyttsx3 # For speak() function. pyttsx3 is the module for conversion of text to speech
 import datetime # for wishMe() function
-import speech_recognition as sr # for takeCommand() function
+import speech_recognition as sr # for takeCommand() function. speech_recognition is the process of converting audio to text
 import wikipedia
 import webbrowser # for opening websites
 import os # for playing music
 from random import randint
 from selenium import webdriver # For Web Scraping
 import pyjokes
+import requests # For weather, news requests module is used for making GET and POST requests
+from pprint import pprint # for weather
+import wolframalpha # For mathematical calculations and general knowledge questions
 
 engine = pyttsx3.init('sapi5') # sapi -> Speech Application Programming Interface (to use inbuilt voice in windows)
 voices = engine.getProperty('voices')
@@ -68,6 +71,21 @@ def openApp(app): # Opens the specified app from our Computer
         files = os.listdir(directory)
         os.startfile(os.path.join(directory, files[files.index(app)]))
     return
+
+def weather_data(query):
+    res=requests.get('http://api.openweathermap.org/data/2.5/weather?'+query+'&APPID=b35975e18dc93725acb092f7272cc6b8&units=metric');
+    return res.json()
+        
+def speak_weather(result,city):
+    print("{}'s temperature: {}°C ".format(city,result['main']['temp']))
+    print("Wind speed: {} m/s".format(result['wind']['speed']))
+    print("Description: {}".format(result['weather'][0]['description']))
+    print("Weather: {}".format(result['weather'][0]['main']))
+    speak("{}'s temperature: {}°C ".format(city,result['main']['temp']))
+    speak("Wind speed: {} m/s".format(result['wind']['speed']))
+    speak("Description: {}".format(result['weather'][0]['description']))
+    speak("Weather: {}".format(result['weather'][0]['main']))
+    return
     
 if (__name__ == "__main__"):
     wishMe()
@@ -80,24 +98,23 @@ if (__name__ == "__main__"):
             speak("Searching Wikipedia...")
             try:
                 query = query.replace("wikipedia", "") # Delete wikipedia from the query string
-                results = wikipedia.summary(query)
+                results = wikipedia.summary(query, sentences = 3)
                 speak("According to Wikipedia...")
-                #print(results)
-                speak(results, sentences = 3)
+                speak(results)
             except:
                 speak("Sorry. No results found")
 
-        elif 'google' in query:
+        elif 'google' in query: # Performs a google search. Give command as Search on Google
             speak("sir. what should i search on google")
             cm =takeCommand().lower()
             webbrowser.open('https://google.com/?#q='+ cm)
 
-        elif 'youtube' in query:
+        elif 'youtube' in query: # Performs a youtube search. GIve command as Search on Youtube
             speak("What should I search on youtube")
             command = takeCommand().lower()
             webbrowser.open('https://www.youtube.com/results?search_query= ' + command)
 
-        elif "facebook" in query:
+        elif "facebook" in query: # Logs in to Facebook. GIve command as Log in to facebook please
             driver = webdriver.Chrome("Your_Driver_Path")
             driver.get("https://facebook.com")
             searchbox = driver.find_element_by_xpath('//*[@id="email"]')
@@ -107,7 +124,7 @@ if (__name__ == "__main__"):
             searchButton = driver.find_element_by_xpath('//*[@id="u_0_b"]')
             searchButton.click()
 
-        elif "outlook" in query:
+        elif "outlook" in query: # Opens Outlook
             driver = webdriver.Chrome("Your_Driver_Path")
             driver.get("https://outlook.live.com")
     
@@ -118,6 +135,32 @@ if (__name__ == "__main__"):
             searchbox.send_keys("Your_Email")
 
             searchButton = driver.find_element_by_xpath('//*[@id="idSIButton9"]')
+            searchButton.click()
+
+        elif "moodle" in query: # Command : Take me to moodle Jarvis
+            driver = webdriver.Chrome("Your_Driver_Path")
+            driver.get("https://moodle.coep.org.in")
+
+            searchbox = driver.find_element_by_xpath('//*[@id="username"]')
+            searchbox.send_keys("Your_MIS")
+
+            searchPassword = driver.find_element_by_xpath('//*[@id="password"]')
+            searchPassword.send_keys("Your_Password")
+
+            searchButton = driver.find_element_by_xpath('//*[@id="loginbtn"]')
+            searchButton.click()
+
+        elif "mis" in query: # Command : Open mis portal Jarvis
+            driver = webdriver.Chrome("Your_Driver_Path")
+            driver.get("http://portal.coep.org.in:9093/SignUp?ReturnUrl=%2f")
+
+            searchbox = driver.find_element_by_xpath('//*[@id="UserName"]')
+            searchbox.send_keys("Your_MIS")
+
+            searchPassword = driver.find_element_by_xpath('//*[@id="Password"]')
+            searchPassword.send_keys("Your_Password")
+
+            searchButton = driver.find_element_by_xpath('//*[@id="btnSignIn"]')
             searchButton.click()
 
         elif "play music" in query:
@@ -132,7 +175,43 @@ if (__name__ == "__main__"):
         elif "thank you" in query:
             speak("You are Welcome!!")
 
-        elif "day" in query:
+        elif "weather" in query:
+            speak("please tell me your city name")
+            city = takeCommand()
+            #city = "mumbai"
+            print()
+            try:
+                query='q='+city;
+                w_data=weather_data(query);
+                speak_weather(w_data, city)
+                print()
+            except:
+                speak('City name not found')
+
+        elif "news" in query:
+            # BBC news api 
+            main_url = " https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=YOUR_API_ID_HERE"
+  
+            # fetching data in json format 
+            open_bbc_page = requests.get(main_url).json() 
+      
+            # getting all articles in a string article 
+            article = open_bbc_page["articles"] 
+  
+            # empty list which will  
+            # contain all trending news 
+            results = [] 
+              
+            for ar in article: 
+                results.append(ar["title"]) 
+                  
+            for i in range(len(results)): 
+                  
+                # printing all trending news
+                print(results[i])
+                speak(results[i])
+
+        elif "day" in query: # What day is it today
             day = datetime.datetime.today().weekday() + 1
       
             #this line tells us about the number  
@@ -150,7 +229,7 @@ if (__name__ == "__main__"):
             speak("The time is")
             speak(strTime)
 
-        elif "date" in query:
+        elif "date" in query: # Tell me what date it is Jarvis
             strDate = str(datetime.date.today())
             speak("The date is")
             speak(strDate)
@@ -191,7 +270,7 @@ if (__name__ == "__main__"):
         elif "write a note" in query: 
             speak("What should i write, sir") 
             note = takeCommand() 
-            file = open('jarvis.txt', 'a') 
+            file = open('ntoes.txt', 'a') 
             speak("Sir, Should i include date and time") 
             snfm = takeCommand() 
             if 'yes' in snfm or 'sure' in snfm: 
@@ -205,8 +284,20 @@ if (__name__ == "__main__"):
           
         elif "show note" in query: 
             speak("Showing Notes") 
-            file = open("jarvis.txt", "r")  
-            print(file.read()) 
+            file = open("notes.txt", "r")  
+            string = file.read()
+            print(string)
+            speak(string)
+
+        elif "delete note" in query:
+            speak("Which note do you want to delete?")
+            delete = takeCommand()
+            with open("notes.txt", "r") as f:
+                lines = f.readlines()
+            with open("notes.txt", "w") as f:
+                for line in lines:
+                    if delete not in line:
+                        f.write(line)
 
         elif "joke" in query:
             speak(pyjokes.get_joke())
@@ -215,3 +306,13 @@ if (__name__ == "__main__"):
             speak("It was nice serving you...")
             speak("Goodbye... See you soon, have a nice day ahead")
             break
+
+        elif ("calculate" in query) or ("who is" in query) or ("what is" in query): # Calculate sin(30), What is Capital Of India, who is president of India, US
+            app_id = 'YOUR_API_ID_HERE'
+            client = wolframalpha.Client(app_id)
+            res = client.query(query)
+            try: 
+                print (next(res.results).text) 
+                speak (next(res.results).text) 
+            except StopIteration: 
+                speak("No results")
